98b10cb4303f4b928448d80aa621128d2ce19e94
RAHUL <dasr16583@gmail.com>
2023-07-10 22:01:27 +0530

    solve some error

--- script.js
+++ script.js
@@ -1,55 +1,14 @@
 
-// animation 
-function stars() {
-  let count = 50;
-  let scene = document.querySelector(".scene");
-  let i = 0;
-  while (i < count) {
-    let star = document.createElement("i");
-    let x = Math.floor(Math.random() * window.innerWidth);
-    let duration = Math.random() * 1;
-    let h = Math.random() * 100;
-
-    star.style.left = x + "px";
-    star.style.width = "1px";
-    star.style.height = 50 + h + "px";
-    star.style.animationDuration = duration + "s";
-
-    scene.appendChild(star);
-    i++;
-  }
-}
-stars();
-
-function stars() {
-  let count = 50;
-  let scene = document.querySelector(".scene");
-  let i = 0;
-  while (i < count) {
-    let star = document.createElement("i");
-    let x = Math.floor(Math.random() * window.innerWidth);
-    let duration = Math.random() * 1;
-    let h = Math.random() * 100;
-
-    star.style.left = x + "px";
-    star.style.width = "1px";
-    star.style.height = 50 + h + "px";
-    star.style.animationDuration = duration + "s";
-
-    scene.appendChild(star);
-    i++;
-  }
-}
-stars();
-
-
-function startAnimation() {
-  let loadingPage = document.getElementById('loading-page');
-  loadingPage.classList.add('animate');
-
-  // Wait for the animation to complete (adjust the duration as needed)
-  setTimeout(function () {
-    // Redirect to the new page after animation completes
-    window.location.href = 'newpage.html';
-  }, 2000); // 2 seconds (adjust the duration as needed)
+function openRocket() {
+  const xhr = new XMLHttpRequest();
+  xhr.open("GET", "/rocket.html", true);
+  xhr.onreadystatechange = () => {
+    if (xhr.readyState === 4 && xhr.status === 200) {
+      document.documentElement.innerHTML = xhr.responseText;
+      setTimeout(() => {
+        window.location.href = "/sec-page/second.html";
+      }, 5000);
+    }
+  };
+  xhr.send();
 }
