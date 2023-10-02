5acbea3a578604f7eb8326936d4390e7e8438348
ozasatyam <satyamoza@outlook.com>
2023-10-02 11:49:45 -0600

    Input field fixed in registation page that only accept image type

--- UI/Register.js
+++ UI/Register.js
@@ -1,33 +1,43 @@
 const form = document.querySelector('.form form'),
-submitbtn = form.querySelector('.submit input'),
-errortxt = form.querySelector('.error-text');
-let fname= document.getElementById("fname");
-let lname= document.getElementById("lname");
-let email= document.getElementById("email");
-let image= document.getElementById("image");
-let phone= document.getElementById("phone");
-let password= document.getElementById("password");
-let cpassword= document.getElementById("cpassword");
+    submitbtn = form.querySelector('.submit input'),
+    errortxt = form.querySelector('.error-text');
+let fname = document.getElementById("fname");
+let lname = document.getElementById("lname");
+let email = document.getElementById("email");
+let image = document.getElementById("image");
+let phone = document.getElementById("phone");
+let password = document.getElementById("password");
+let cpassword = document.getElementById("cpassword");
 form.onsubmit = (e) => {
     e.preventDefault();
 }
+$('#image').on("change", function (value) {
 
-submitbtn.onclick = () =>{
+    var fileExtension = ['jpeg', 'jpg', 'png', '.svg'];
+    if ($.inArray($(this).val().split('.').pop().toLowerCase(), fileExtension) == -1) {
+        alert("Only '.jpeg','.jpg', '.png' '.svg' formats are allowed.");
+        image.value = []
+    } else {
+        console.log($(this).val())
+        image.value = $(this).val()
+    }
+})
+
+
+submitbtn.onclick = () => {
     //if we buy domain then remove this
-    if(fname.value.length>0 && lname.value.length>0 && email.value.length>0 && image.value.length>0 && phone.value.length>0 && password.value.length>0 && cpassword.value.length>0){
-        if(password.value == cpassword.value){
+    if (fname.value.length > 0 && lname.value.length > 0 && email.value.length > 0 && image.value.length > 0 && phone.value.length > 0 && password.value.length > 0 && cpassword.value.length > 0) {
+        if (password.value == cpassword.value) {
             alert("Registration Successfull");
-            location.href="../indexafter.html";
+            location.href = "../indexafter.html";
         }
-        else
-        {
+        else {
             alert("Passwords are not matching please check");
         }
     }
-    else
-        {
-            alert("All field are required");
-        }
+    else {
+        alert("All field are required");
+    }
     //--------
     /*let xhr = new XMLHttpRequest();    
     xhr.open("POST","./UI/Register.php",true);
